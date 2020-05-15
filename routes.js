d1339bd5c0f026bd1a2def01b393f9d5acd92c33
Guilherme <guilherme_devz@hotmail.com>
2020-05-14 21:51:04 -0300

    starting form

--- routes.js
+++ routes.js
@@ -2,11 +2,19 @@ const express = require("express")
 const routes = express.Router()
 
 routes.get("/", (req, res) => {
-    return res.render("index")
+    return res.redirect("/instructors")
 })
 
-routes.get("/portfolio", (req, res) => {
-    return res.redirect("/")
+routes.get("/instructors", (req, res) => {
+    return res.render("instructors/index")
+})
+
+routes.get("/members", (req, res) => {
+    return res.send("members")
+})
+
+routes.get("/instructors/create", (req, res) => {
+    return res.render("instructors/create")
 })
 
 module.exports = routes
